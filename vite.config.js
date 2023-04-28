34d28992387ac0b44107870fe3f1339871122334
Christer Aarø <christeraaro@gmail.com>
2023-04-28 12:55:50 +0200

    Vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0, // 0 means all files will be inlined
+  }
 })
